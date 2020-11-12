d3752d672ec50aa27100faf1918d6faa994695c3
JR Lherm <lherm.jr@gmail.com>
2020-11-12 16:01:09 +0100

    Enable addind doses + Design of the show page

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,27 @@
 class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+    @dose.cocktail = @cocktail
+  end
+
+  def create
+    # raise
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
